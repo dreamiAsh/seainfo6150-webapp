046d6fdc2155f12fd1b37aca7796874eb93f9bbc
dreamiAsh <ashlyn.leng@gmail.com>
2020-11-03 16:44:49 -0800

    responsive hw

--- src/ArticleList/ArticleList.jsx
+++ src/ArticleList/ArticleList.jsx
@@ -4,28 +4,13 @@ import ArticleListItem from "../ArticleListItem/ArticleListItem";
 
 const ArticleList = (props) => {
     const myList = props.list
-    var list3 = [];
-    var styleFlag = true;
 
-    var box = [];
-
-    function list3BoxStyle(){
-      console.log(styleFlag)
-      styleFlag = !styleFlag;
-    }
-
-    const mappedList = myList.map((listItem) => {
-      if(list3.length < 3)
-        list3.push(<li><ArticleListItem article = {listItem}/></li>)
-      else {
-        box.push(<div className={styleFlag === true ? styles.style1 : styles.style2} onClickCapture={list3BoxStyle.bind(this)} 
-                  >{list3}</div>)
-        list3 = [];
-      }
-    })
+    const mappedList = myList.map((listItem) => (
+      <li><ArticleListItem article = {listItem}/></li>
+    ))
   return (
     <article className={styles.container}>
-        <ul className={styles.list}>{box}</ul>
+        <ul className={styles.list}>{mappedList}</ul>
     </article>
   );
 };
